fbb88786d225b7b8a89698772d005f3a360956c8
Apungu Raymond <apunguray@gmail.com>
2022-07-11 18:08:30 +0800

    commits

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import BasicInfo from "./BasicInfo";
 import Address from "./Address";
 import LastPage from "./LastPage";
+import OtherInformation from "./OtherInformation";
 
 export {
     BasicInfo,
     Address,
-    LastPage
+    LastPage,
+    OtherInformation
 }
