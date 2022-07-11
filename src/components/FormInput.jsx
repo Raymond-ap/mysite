3a2f51eefbe3d73f2dc4571c701050eeab1a085a
Raymond-ap <apunguray@gmail.com>
2022-07-11 09:53:39 +0800

    reformat to typescript

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FormInput = ({ placeholder, label, value, type, onChange }) => {
+  return (
+    <div className="input-wrapper">
+      <p className="label">{label}</p>
+      <input
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
