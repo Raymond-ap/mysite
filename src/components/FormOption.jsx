9743757bac4b37343bde98b56f15ba5f89894473
Apungu Raymond <apunguray@gmail.com>
2022-07-11 18:08:43 +0800

    modified select options

--- src/components/FormOption.jsx
+++ src/components/FormOption.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
-const FormOption = ({label, value}) => {
+const FormOption = ({label, value, options}) => {
   return (
     <div className="input-wrapper">
       <p className="text-sm font-semibold mb-2 text-slate-400 capitalize">
         {label}
       </p>
      <select className="w-full rounded-md border-2 text-slate-400 border-slate-400 p-1 outline-none">
-        <option value={value}>{value}</option>
+        {options ? options.map((option, index) => {
+          return (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          );
+        }
+        ): (
+          <option value={value}>{value}</option>
+        )}
       </select>
     </div>
   );
