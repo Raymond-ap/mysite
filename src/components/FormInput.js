431a8d852e7d33e10501780690e9f6e715745d04
Raymond-ap <apunguray@gmail.com>
2022-07-11 09:40:45 +0800

    committing changes

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const FormInput = () =>{
   return (
-    <div>
-      <h1>FormInput</h1>
-    </div>
+    <div className='input-wrapper'>
+    <input className='input'/>
+  </div> 
   )
 }
 
