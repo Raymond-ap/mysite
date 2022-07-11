60f0885752e0b5575f5445f8590cea564d44a4e1
Raymond-ap <apunguray@gmail.com>
2022-07-11 09:40:33 +0800

    initial commit

---
+++ src/pages/BasicInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FormInput } from '../components'
+
+const BasicInfo = () => {
+    return (
+        <div className='container'>
+            <FormInput/>
+        </div>
+    )
+}
+
+export default BasicInfo
