47b2b6f998a1308654a7232cd436d81c81f23433
Apungu Raymond <apunguray@gmail.com>
2022-07-15 23:55:31 +0800

    committing changes

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,11 @@
 import FormInput from "./FormInput";
 import FormOption from './FormOption'
 import PersonalInformation from './PersonalInformation'
+import CompanyInformation from './CompanyInformation'
 
 export {
     FormInput,
     FormOption,
-    PersonalInformation
+    PersonalInformation,
+    CompanyInformation
 }
