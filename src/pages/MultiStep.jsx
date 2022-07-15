36e3d782ed9ea0e25594e70660966f56d6e52b04
Apungu Raymond <apunguray@gmail.com>
2022-07-15 22:52:16 +0800

    render parent component

---
+++ src/pages/MultiStep.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+import { PersonalInformation } from "../components";
+
+const MultiStep = () => {
+  return (
+    <div className="w-screen flex flex-row  h-screen bg-white">
+      <div className="bg-cyan-500 h-screen basis-1/4 p-10">
+        <div className="font-bold text-2xl mb-6 from-neutral-900">
+          <h1>Logo here</h1>
+        </div>
+      </div>
+      <div className="p-10 mr-36 ml-10 justify-center w-2/3 content-center ">
+       <PersonalInformation/>
+      </div>
+    </div>
+  );
+};
+
+export default MultiStep;
